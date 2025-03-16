beea0429d873d1b45202ee1a459889140f83694f
EMarines <61839920+EMarines@users.noreply.github.com>
2025-03-16 13:30:44 -0600

    Fix: Solucionar error de HMR en Vercel ajustando configuraciones de Svelte y Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	// Configuración global para Vite
+	build: {
+		// Asegurarse de que no hay configuraciones que puedan causar problemas
+		sourcemap: true
+	}
 });
