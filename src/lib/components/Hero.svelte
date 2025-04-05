f0bd493f96cc0d36fd5d3c681f46f30dc306b97b
EMarines <61839920+EMarines@users.noreply.github.com>
2025-04-05 10:55:37 -0600

    fix: improve accessibility by adding proper frame titles and fixing heading hierarchy

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -9,10 +9,9 @@
                 <img src={logo} alt="Match Home">
             </div>
             <div>
-                <h2>Match Home</h2>
-                <h5>¡Tu Patrimonio En Buenas Manos!</h5>
+                <h1>Match Home</h1>
+                <h2 class="subtitle">¡Tu Patrimonio En Buenas Manos!</h2>
             </div>
-
         </div>
     </div>
 </div>
@@ -21,22 +20,22 @@
     .hero{
         padding: 0.5em 0;
         background-color: var(--primary-color);
-        /* color: white; */
     }
 
-    h2{
+    h1 {
         font-size: 3.5em;
         font-weight: 400;
         margin: 0;
     }
 
-    h5 {
+    h2.subtitle {
         font-size: 1.5em;
         font-weight: 150;
+        margin: 0;
     }
 
-    img{
-      width: 180px;  
+    img {
+        width: 180px;  
     }
 
     .header {
@@ -56,15 +55,15 @@
 
     @media (max-width: 800px){
         .header {
-            display: felx;
+            display: flex;
             flex-direction: column;
             align-items: center;
             justify-content: center;
         }
-        h2 {
+        h1 {
             font-size: 2.5rem;
         }
-        h5 {
+        h2.subtitle {
             font-size: 1.2rem;
         }
     }
